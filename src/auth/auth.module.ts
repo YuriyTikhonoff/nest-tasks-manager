ed58f312520c3016196707f8f0e2942381b5b47f
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-01-26 20:12:22 +0200

    Add jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,19 +5,22 @@ import { User } from "./user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
+import { JWT_EXPIRATION_TIME, JWT_SECRET } from "src/constants";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.register({
-      secret: "topSecret51",
+      secret: JWT_SECRET,
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: JWT_EXPIRATION_TIME,
       },
     }),
     TypeOrmModule.forFeature([User]),
   ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [PassportModule, JwtStrategy],
 })
 export class AuthModule {}
--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,6 @@ export const TYPE_ORM_DATA_SOURCE = "DATA_SOURCE";
 export const DB_ERROR_STATUS_CODES = {
   DUPLICATE_VALUE: "23505",
 };
+
+export const JWT_SECRET = "topSecret51";
+export const JWT_EXPIRATION_TIME = 3600;
